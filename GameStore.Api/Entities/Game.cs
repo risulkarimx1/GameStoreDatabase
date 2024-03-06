ae61b577d4fdd76850a56548a2cabea75384ccd6
Risul <risulkarimx1@gmail.com>
2024-03-07 01:22:31 +0600

    added data migration

---
+++ GameStore.Api/Entities/Game.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Entities;
+
+public class Game
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public int GenreId { get; set; }
+
+    [Required]
+    public Genre Genre { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+
+    [Required]
+    public DateOnly ReleaseDate { get; set; }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,15 @@
+using GameStore.Api.Data;
 using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
 var app = builder.Build();
 
 app = app.MapGamesEndPoints();
+
+app.MigrateDb();
 
 app.Run();
