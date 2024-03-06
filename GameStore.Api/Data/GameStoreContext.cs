644ecbf2cbc1b7b49360ecf4fc729527919ba739
Risul <risulkarimx1@gmail.com>
2024-03-07 01:31:27 +0600

    added genre seeds

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -7,4 +7,14 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options): DbCon
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new Genre { Id = 1, Name = "Action" },
+            new Genre { Id = 2, Name = "Adventure" },
+            new Genre { Id = 3, Name = "RPG" },
+            new Genre { Id = 4, Name = "Strategy" }
+        );
+    }
 }
